600184767b4e658eca6a26413ef3772e23a3cf51
Maya Imhoff <mayelyimhoff@gmail.com>
2021-03-21 13:41:00 +0100

    menu for mobile, tablet and desktop

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li')
 
+  burger.addEventListener('click', () => {
+    // to toggle or click the navigation
+    nav.classList.toggle('nav-active');
+
+    // to sexyly animate the sliding in of menu items:
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+      // so that animation doesn't stop after we open menu for first time
+        link.style.animation = ''
+      } else {
+      // navbar links come in with slight delay after one another and that's waht we need the index for
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+    burger.classList.toggle('menu-icon');
+
+  });
+
+}
+
+navSlide();
