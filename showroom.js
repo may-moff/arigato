f6a9008da27634932246656aec7034e8d5ffa8ab
MarekHalaczkiewicz <emhala8@gmail.com>
2021-03-26 10:27:18 +0100

    Changed pics position, style. Final

--- showroom.js
+++ showroom.js
@@ -4,8 +4,12 @@ let currentSlide = 1;
 const slides = document.getElementsByClassName("carousel-Img");
 
 function showSlide(slideIndex) {
-  if (slideIndex > slides.length) { currentSlide = 1} 
-  if (slideIndex < 1) { currentSlide = slides.length }
+  if (slideIndex > slides.length){ 
+    currentSlide = 1
+  } 
+  if (slideIndex < 1) {
+     currentSlide = slides.length 
+    }
   for ( let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none'
   }
